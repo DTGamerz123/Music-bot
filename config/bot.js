24b9ab7095cb8c65f186f5ad6a6aef51e3001277
DTGamerz123 <77567986+DTGamerz123@users.noreply.github.com>
2021-03-04 18:42:06 +0530

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'Nzg1MzY3Njg0OTYzMjM3OTE4.X820pw.wjQn2PbqeZ9fVVW9xM8Ra03viMQ',
+        prefix: 'M!',
+        activity: 'Playing Some Music For Fun and listening to M!help',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
